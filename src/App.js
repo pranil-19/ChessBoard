e5cd5534144627aea00c27a2a192765af2c59724
Kousik Roy <kaushikroy1001@gmail.com>
2022-03-26 01:13:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ChessBoard from './ChessBoard';
+
+function App() {
+  return (
+    <div>
+
+      <h3>Click on any point and it will show all the possible move that a Knight can make throught that position!</h3>
+      <ChessBoard />
+
+    </div>
+  );
+}
+
+export default App;
